0cb22d7399a708669c898fb5e0132da2249204db
Ellen Nagasawa <84338695+ellennagasawa@users.noreply.github.com>
2026-08-24 13:42:46 -0400

    Add files via upload

---
+++ fast-portuguese-vocabulary-github-pages/data/unit5.js
@@ -0,0 +1,6 @@
+window.FAST_PORTUGUESE_DATASETS = window.FAST_PORTUGUESE_DATASETS || {};
+window.FAST_PORTUGUESE_DATASETS["unit5"] = {
+  unit: 5,
+  title: "Ao meu redor",
+  cards: []
+};
---
+++ fast-portuguese-vocabulary-github-pages/data/unit6.js
@@ -0,0 +1,6 @@
+window.FAST_PORTUGUESE_DATASETS = window.FAST_PORTUGUESE_DATASETS || {};
+window.FAST_PORTUGUESE_DATASETS["unit6"] = {
+  unit: 6,
+  title: "Faces da lusofonia",
+  cards: []
+};
